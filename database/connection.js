7c040742196dc1ed22c8ed1c886a2161db9f1a38
George <nuriszka@gmail.com>
2022-08-05 09:24:34 +0200

    added product model

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,8 @@ require('dotenv').config({path: './.env'});
 
 module.exports = async function connect(){
    return await mongoose.connect(process.env.CONNECTION_STRING, {
-        dbName: 'node_api'
+        dbName: 'node_api',
+       useNewUrlParser: true
     })
 }
 
