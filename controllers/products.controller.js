cf40c0741157f019806db8861f00db1a1d1d5de3
George <nuriszka@gmail.com>
2022-08-07 20:20:55 +0200

    refactored product service to centralize handling responses

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -2,64 +2,66 @@ const productService = require('../services/products.service');
 
 module.exports.handleAddProduct = function(req, res) {
    productService.addProduct(req.body)
-       .then(product => {
-           return res.status(201).json({
-               message: 'product added successfully',
-               status: 201,
-               body: product
-           });
-       })
-       .catch(error => {
-           return res.status(error.status).send({
-                message: 'something went wrong while performing the operation',
-               status: 500,
-           })
-       });
+       .then(data => handleSuccessResponse(req, res, data))
+       .catch(err => handleDatabaseErrorResponse(res, err));
 }
 
 module.exports.handleGetAllProducts = function (req, res){
-    productService.fetchAllProducts(req.query).then(data => handleSingleProductSuccessResponse(res, data))
-        .catch(err => handleSingleProductErrorResponse(res, err))
+    productService.fetchAllProducts(req.query)
+        .then(data => handleSuccessResponse(req, res, data))
+        .catch(err => handleDatabaseErrorResponse(res, err))
 }
 
 module.exports.handleGetProductById = function (req, res){
     const id  = req.params['id'];
     productService.fetchById(id)
-        .then(data => handleSingleProductSuccessResponse(res, data))
-        .catch(err => handleSingleProductErrorResponse(res, err));
+        .then(data => handleSuccessResponse(req, res, data))
+        .catch(err => handleDatabaseErrorResponse(res, err));
 }
 
 module.exports.handleUpdateProductById = function(req, res){
     const id = req.params['id'];
     productService.updateProduct(id, req.body)
-        .then(data => handleSingleProductSuccessResponse(res, data))
-        .catch(err => handleSingleProductErrorResponse(res, err))
+        .then(data => handleSuccessResponse(req, res, data))
+        .catch(err => handleDatabaseErrorResponse(res, err))
 }
 
 
 module.exports.handleDeleteProductById = function(req, res){
     const id = req.params['id'];
     productService.deleteById(id)
-        .then(data => handleSingleProductSuccessResponse(res, data))
-        .catch(err => handleSingleProductErrorResponse(res, err))
+        .then(data => handleSuccessResponse(req, res, data))
+        .catch(err => handleDatabaseErrorResponse(res, err))
 }
 
-const handleSingleProductSuccessResponse = (res, data) => {
-    if(data) {
+const handleSuccessResponse = (req, res, data) => {
+    const httpMethod = req.method;
+    if(data && httpMethod === 'GET'){
         return res.status(200).json({
             body: data,
             message: 'operation performed successfully',
             status: 200
         })
     }
+    if(data && httpMethod === 'POST'){
+        return res.status(201).json({
+            message: 'product added successfully',
+            status: 201,
+            body: data
+        });
+    }
+
+    if(data && (httpMethod === 'PUT' || httpMethod === 'DELETE')) {
+        return res.status(204).json();
+    }
 
      return res.json({
-        message: 'could not find the requested product',
+        message: 'could not find the requested product(s)',
         status: 404,
     })
 }
 
-const handleSingleProductErrorResponse = function(res, error){
+const handleDatabaseErrorResponse = function(res, error){
     return res.status(500).json({
         message: 'something went wrong while performing the database operation',
         status: 500,
