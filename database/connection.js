bb1fe10ea8777f43dd1e3724fb69c08a3752d116
George Maina <george.maina@finova.tech>
2022-08-08 15:03:36 +0200

    moved product seeding population to connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,21 @@
-const mongoose = require('mongoose');
-require('dotenv').config({path: './.env'});
+const mongoose = require("mongoose");
+const Product = require("./models/product.model");
+const seedProductData = require("./seedData/product.seedData");
 
-module.exports = async function connect(){
-   return await mongoose.connect(process.env.CONNECTION_STRING, {
-        dbName: 'node_api',
-       useNewUrlParser: true
+require("dotenv").config({ path: "./.env" });
+
+module.exports = async function connect() {
+  return await mongoose
+    .connect(process.env.CONNECTION_STRING, {
+      dbName: "node_api",
+      useNewUrlParser: true,
     })
-}
-
+    .then(() => {
+      return Product.countDocuments({}).exec();
+    })
+    .then((count) => {
+      if (count < 3) {
+        seedProductData();
+      }
+    });
+};
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const cors = require("cors");
 
 const productRouter = require("./routers/products.router");
 const connectDatabase = require("./database/connection");
-const Product = require("./database/models/product.model");
-const seedProductData = require('./database/seedData/product.seedData')
 
 const app = express();
 app.use(cors());
@@ -15,14 +13,6 @@ app.use(express.json());
 app.use(`${process.env.API_VERSION}/products`, productRouter);
 
 connectDatabase()
-  .then(() => {
-    return Product.countDocuments({}).exec();
-  })
-  .then((count) => {
-    if (count < 3) {
-        seedProductData();
-    }
-  })
   .then(() => {
     app.listen(process.env.PORT, function () {
       console.log(`up and running on port ${process.env.PORT}`);
